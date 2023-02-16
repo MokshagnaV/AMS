d8349bc0ae68d7496bf8682a4c16d1585813c7d7
vikasbanda <vikasreddy150@gmail.com>
2023-02-16 14:58:58 +0530

    Misc

--- owner/urls.py
+++ owner/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/', views.signup.as_view(), name='index')
+    path('signup/', views.signup.as_view(), name='signup'),
+    path('login/', views.login.as_view(), name="login")
 ]
 

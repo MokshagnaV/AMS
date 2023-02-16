dc92970adb557c887dca35784fd46a2cdfabab18
MokshagnaV <mokshagnavemula@gmail.com>
2023-02-16 14:42:39 +0530

    Add Basic model

--- owner/models.py
+++ owner/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class owner(models.Model):
+    password = models.CharField(max_length=20)
+    name = models.CharField(max_length=100)
+    floor_no = models.IntegerField()
+    flat_no = models.IntegerField()
+    contact = models.IntegerField()
+    email = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.name},{self.flat_no}"
--- owner/views.py
+++ owner/views.py
@@ -1,9 +1,35 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from django.views import View
+from .models import owner
 # Create your views here.
 
 class signup(View):
     def post(self, request):
-        pass
+        form = request.POST['Yes']
+        if form == 'Submit':
+            name_ = request.POST['fname'] + request.POST['lname']
+            email_ = request.POST['email']
+            contact_ = request.POST['phnumber']
+            password_ = request.POST['pass']
+            flatno = request.POST['flatno']
+            floorno = request.POST['floorno']
+            owner_details = owner(
+                password = password_,
+                name = name_,
+                email = email_,
+                contact = contact_,
+                flat_no = flatno,   
+                floor_no = floorno
+            )
+            owner_details.save()
+
+            return HttpResponseRedirect("/owner/login")
+        return render(request, 'owner/login.html')
     def get(self, request):
         return render(request, 'owner/signup.html')
+
+class login(View):
+    def post(self, request):
+        pass
+    def get(self, request):
+        return render(request, 'owner/login.html')

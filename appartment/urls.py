833f258a70d8e511bb75044967952313e18e0b45
MokshagnaV <mokshagnavemula@gmail.com>
2023-02-27 18:50:39 +0530

    Add main page

--- appartment/urls.py
+++ appartment/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('owner/', include("owner.urls")),
     path('association/', include("association.urls")),
+    path('', include("main.urls")),
 ]

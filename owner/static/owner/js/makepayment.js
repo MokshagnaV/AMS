9a9d09333681d7986a598cb6e61af62c2f3faa0b
MokshagnaV <mokshagnavemula@gmail.com>
2023-03-04 00:26:48 +0530

    All JS for payment_desc displaying

--- owner/static/owner/js/makepayment.js
+++ owner/static/owner/js/makepayment.js
@@ -1,14 +1,29 @@
 const pay_mode = document.querySelector('#payment_mode')
-        const utr_label = document.querySelector('#utr_label')
-        const utr = document.querySelector('#utr')
-        console.log(pay_mode, utr, utr_label)
-        pay_mode.addEventListener('change', () => {
-            if(pay_mode.value == "Offline"){
-                utr_label.style.display = "none";
-                utr.style.display = "none";
-            }
-            if(pay_mode.value == "Online"){
-                utr_label.style.display = "block";
-                utr.style.display = "block";
-            }
-        })
+const utr_label = document.querySelector('#utr_label')
+const utr = document.querySelector('#utr')
+pay_mode.addEventListener('change', () => {
+    if (pay_mode.value == "OF") {
+        utr_label.style.display = "none";
+        utr.style.display = "none";
+    }
+    if (pay_mode.value == "ON") {
+        utr_label.style.display = "block";
+        utr.style.display = "block";
+    }
+})
+
+const pay_for = document.querySelector("#pay_for");
+const desc_label = document.querySelector("#pay_desc");
+const desc = document.querySelector("#desc")
+desc_label.style.display = "none";
+        desc.style.display = "none";
+console.log(pay_for, desc, desc_label)
+pay_for.addEventListener("change", ()=>{
+    if(pay_for.value == "OB"){
+        desc_label.style.display = "block";
+        desc.style.display = "block";
+    }else{
+        desc_label.style.display = "none";
+        desc.style.display = "none";
+    }
+})

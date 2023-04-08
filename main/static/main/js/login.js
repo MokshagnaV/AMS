de7f55d237efa79b87380e15d3c7660cf7bbea5a
MokshagnaV <mokshagnavemula@gmail.com>
2023-04-08 12:24:46 +0530

    password view toggle in login page

--- main/static/main/js/login.js
+++ main/static/main/js/login.js
@@ -12,3 +12,28 @@ login.addEventListener("click", () => {
 	slider.classList.remove("moveslider");
 	formSection.classList.remove("form-section-move");
 });
+
+const eyes = document.querySelectorAll("#eye");
+const eyes_close = document.querySelectorAll("#eye-close");
+let eyeCheck = true;
+eyes.forEach(eye => {
+    eye.addEventListener("click", ()=>{
+        if(eyeCheck){
+            eye.style.display = "none";
+            eye.nextElementSibling.style.display = "block";
+            eye.previousElementSibling.setAttribute("type", "text");
+            eyeCheck = false;
+        }
+    }
+)})
+
+eyes_close.forEach(eye => {
+    eye.addEventListener("click", ()=>{
+        if(!eyeCheck){
+            eye.style.display = "none";
+            eye.previousElementSibling.style.display = "block";
+            eye.previousElementSibling.previousElementSibling.setAttribute("type", "password");
+            eyeCheck = true;
+        }
+    })
+})
